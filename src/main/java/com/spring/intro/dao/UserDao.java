bd8c9566824234c8419a07074c0f78251115f628
dariiapikul <dariyapikul@gmail.com>
2020-10-19 14:45:29 +0300

    Travis : configured Model : User created, also Dao and Service AppConfig : Spring connectivity configuration pom.xml : dependencies added MainApp : app testing implemented

---
+++ src/main/java/com/spring/intro/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.spring.intro.dao;
+
+import com.spring.intro.model.User;
+import java.util.List;
+
+public interface UserDao {
+    User create(User user);
+
+    List<User> getAll();
+}
---
+++ src/main/java/com/spring/intro/dao/UserDaoImpl.java
@@ -0,0 +1,46 @@
+package com.spring.intro.dao;
+
+import com.spring.intro.model.User;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    private final SessionFactory sessionFactory;
+
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public User create(User user) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return user;
+        } catch (Exception exception) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to create the user - " + user, exception);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM User", User.class).getResultList();
+        }
+    }
+}
